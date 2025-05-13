fa3947e1edc1da5112247df62751f4f6190081c7
Wander Souza <wandersouza@live.com>
2025-05-13 16:39:29 -0300

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
+	site: 'https://wandersouzadev.github.io',
 	legacy: {
 		collections: true
 	}
